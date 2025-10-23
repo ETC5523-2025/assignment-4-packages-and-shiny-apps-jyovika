bd9c5616620ff5416dcf0d62e2bc66dd9d9b5b64
Jyovika Aswale <jasw0002@student.monash.edu>
2025-10-23 20:55:50 +1100

    added examples to plot function

--- R/plot_hai_totals.R
+++ R/plot_hai_totals.R
@@ -2,10 +2,16 @@
 #'
 #' Creates a bar chart of total infections by a chosen variable.
 #'
-#' @param data Data frame (like `hai_data_clean`)
-#' @param by Grouping variable ("Infection_Type", "Age_Group", or "Sex")
+#' @param data Data frame like `hai_data_clean`
+#' @param by One of "Infection_Type", "Age_Group", or "Sex"
+#'
+#' @examples
+#' # data("hai_data_clean", package = "haiInsight")
+#' # plot_hai_totals(hai_data_clean, by = "Infection_Type")
+#' # plot_hai_totals(hai_data_clean, by = "Age_Group")
+#' # plot_hai_totals(hai_data_clean, by = "Sex")
+#'
 #' @export
-#' @importFrom rlang .data
 plot_hai_totals <- function(data, by = "Infection_Type") {
   sums <- summarise_hai(data, by)
   ggplot2::ggplot(sums, ggplot2::aes(x = reorder(.data[[by]], Total), y = Total)) +
